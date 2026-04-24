6e13d693bb1e4d1efc06d3cd0270621a40bc5849
Markus Matoni <markus.matoni@gbv.de>
2026-04-24 09:42:57 +0200

    feat: add initial test setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { fileURLToPath, URL } from "node:url"
+import { version } from "./package.json"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify(version),
+  },
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: ["./test/setup.js"],
+    coverage: {
+      provider: "v8",
+      include: ["src/**"],
+      exclude: ["src/assets/**", "src/main.js"],
+    },
+  },
 })
